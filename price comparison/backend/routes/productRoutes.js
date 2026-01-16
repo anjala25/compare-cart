537e17956be54af7941e8bd5062328326fe79908
Aleenavp <vpaleena7@gmail.com>
2026-01-16 22:15:33 +0530

    Delete price comparison directory

--- price comparison/backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/productController");
-
-router.post("/add", controller.addProduct);
-router.get("/", controller.getProducts);
-router.get("/search", controller.searchProduct);
-
-module.exports = router;
